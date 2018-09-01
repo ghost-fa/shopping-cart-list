55e1d22489beb671286697d35ae022df6f3bb77d
ghost-fk <fayyad_r22@yahoo.com>
2018-09-01 09:24:59 +0200

    add json file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import './styles/main.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+imoprt products from './products.json'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
