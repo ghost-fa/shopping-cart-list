3094c7153c8acd154d2f9cf5a019c8cb04e21cde
ghost-fk <fayyad_r22@yahoo.com>
2018-09-01 18:30:32 +0200

    i  use Spread Attributes to spread the state and add varibal invevtory to add item to the cart

--- src/modules/reducer.js
+++ src/modules/reducer.js
@@ -1,6 +1,21 @@
 export const appReducer = (state = {}, action) => {
+  const { products, cart } = state;
+  const { product } = action;
+  const { id, title, price } = product || {};
+
   switch (action.type) {
     case 'ADD_TO_CART':
+      const inventory = product.inventory;
+      return {
+        ...state,
+        products: {
+          ...products,
+          [id]: {
+            ...product,
+            inventory: inventory - 1
+          }
+        }
+      };
     case 'REMOVE_ITEM':
     case 'REMOVE_ALL_ITEM':
     case 'CHECKOUT':
